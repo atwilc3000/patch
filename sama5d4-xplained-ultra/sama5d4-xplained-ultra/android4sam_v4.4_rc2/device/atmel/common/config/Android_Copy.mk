3250afbd2515523ff7d6736d2d570281001fa942
tony <tony.cho@atmel.com>
2015-03-18 11:07:13 +0900

    Add sama5d4-xult patch

---
+++ sama5d4-xplained-ultra/sama5d4-xplained-ultra/android4sam_v4.4_rc2/device/atmel/common/config/Android_Copy.mk
@@ -0,0 +1,33 @@
+# make file for new hardware  from 
+#PREBUILD_FIRMWARE := realtek
+PREBUILD_FIRMWARE := wilc3000
+LOCAL_PATH := device/atmel/common/config
+
+# system configuration files
+#
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/fstab.sama5-ek:root/fstab.sama5-ek \
+        $(LOCAL_PATH)/fstab.sama5-pda:root/fstab.sama5-pda \
+	$(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
+	$(LOCAL_PATH)/android.conf:system/etc/android.conf \
+	$(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+	$(LOCAL_PATH)/AT42QT1070_QTouch_Sensor.kl:system/usr/keylayout/AT42QT1070_QTouch_Sensor.kl \
+	$(LOCAL_PATH)/egl.cfg:system/lib/egl/egl.cfg \
+	$(LOCAL_PATH)/at91_adc.idc:system/usr/idc/at91_adc.idc \
+        $(LOCAL_PATH)/Atmel_maXTouch_Touchscreen.idc:system/usr/idc/Atmel_maXTouch_Touchscreen.idc     \
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+        $(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf
+
+ifeq ($(PREBUILD_FIRMWARE), realtek)
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wlan.ko:system/lib/modules/wlan.ko \
+	$(LOCAL_PATH)/rtk_btusb.ko:system/lib/modules/rtk_btusb.ko \
+	$(LOCAL_PATH)/rtk8723a:system/etc/firmware/rtk8723a
+endif
+
+ifeq ($(PREBUILD_FIRMWARE), wilc3000)
+	
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wilc3000.ko:system/lib/modules/wilc3000.ko
+endif
---
+++ sama5d4-xplained-ultra/sama5d4-xplained-ultra/android4sam_v4.4_rc2/hardware/atmel/sama5dx/wlan/firmware/Android.mk
@@ -0,0 +1,55 @@
+#ATMEL Johnny
+
+###################################################
+#	wilc3000
+###################################################
+
+LOCAL_PATH:=$(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_firmware.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_firmware_ap.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_firmware_p2p_concurrency.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bt_fw_BR921600_FC_Off.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bt_fw_BR921600_FC_13_21.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bt_fw_BR1375000_FC_13_21.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
